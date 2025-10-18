03bbb042606084da7b972d4b893c114a76b8d1ae
vakou <vakoum2018@gmail.com>
2025-10-18 21:11:20 +0200

    Fixed the issue where baby-versions couldn't spawn. Also tweaked sound a little

--- src/main/java/io/github/plexiglasog/larrymod/entity/custom/LarryEntity.java
+++ src/main/java/io/github/plexiglasog/larrymod/entity/custom/LarryEntity.java
@@ -1,24 +1,34 @@
 package io.github.plexiglasog.larrymod.entity.custom;
 
 import io.github.plexiglasog.larrymod.Larrymod;
-import io.github.plexiglasog.larrymod.entity.LarrymodEntities;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.PassiveEntity;
+
+import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
+
+import net.minecraft.nbt.NbtCompound;
+
+
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.TimeHelper;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,6 +40,12 @@ public class LarryEntity extends HostileEntity implements Angerable {
 
     private static final UniformIntProvider ANGER_PASSING_COOLDOWN_RANGE = TimeHelper.betweenSeconds(4, 6);
     private static final UniformIntProvider ANGER_TIME_RANGE = TimeHelper.betweenSeconds(20, 39);
+
+    private static final TrackedData<Boolean> BABY = DataTracker.registerData(LarryEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    private static final Identifier BABY_SPEED_MODIFIER_ID = Identifier.ofVanilla("baby");
+    private static final EntityAttributeModifier BABY_SPEED_BONUS = new EntityAttributeModifier(
+            BABY_SPEED_MODIFIER_ID, 0.5, EntityAttributeModifier.Operation.ADD_MULTIPLIED_BASE
+    );
     @Nullable
     private UUID angryAt;
     private int angerPassingCooldown;
@@ -72,8 +88,42 @@ public class LarryEntity extends HostileEntity implements Angerable {
     }
 
     @Override
+    public void setBaby(boolean baby) {
+        this.getDataTracker().set(BABY, baby);
+        if (this.getWorld() != null && !this.getWorld().isClient) {
+            EntityAttributeInstance entityAttributeInstance = this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+            entityAttributeInstance.removeModifier(BABY_SPEED_MODIFIER_ID);
+            if (baby) {
+                entityAttributeInstance.addTemporaryModifier(BABY_SPEED_BONUS);
+            }
+        }
+    }
+
+    @Override
+    protected void initDataTracker(DataTracker.Builder builder) {
+        super.initDataTracker(builder);
+        builder.add(BABY, false);
+    }
+
+    @Override
+    public boolean isBaby() {
+        return this.getDataTracker().get(BABY);
+    }
+
+    @Override
     protected @Nullable SoundEvent getAmbientSound() {
         return Larrymod.LARRY_SOUND_EVENT;
+    }
+
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return super.getHurtSound(source);
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return super.getDeathSound();
     }
 
     @Override
@@ -139,8 +189,50 @@ public class LarryEntity extends HostileEntity implements Angerable {
         }
     }
 
+    public static boolean shouldBeBaby(Random random) {
+        return random.nextFloat() < 0.05F;
+    }
+
     @Override
     public void chooseRandomAngerTime() {
         this.setAngerTime(ANGER_TIME_RANGE.get(this.random));
+    }
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putBoolean("IsBaby", this.isBaby());
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.setBaby(nbt.getBoolean("IsBaby"));
+    }
+
+    @Nullable
+    @Override
+    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData) {
+        Random random = world.getRandom();
+        entityData = super.initialize(world, difficulty, spawnReason, entityData);
+        if (entityData == null) {
+            entityData = new LarryEntity.LarryData(shouldBeBaby(random));
+        }
+        if (entityData instanceof LarryEntity.LarryData larryData) {
+            if (larryData.baby) {
+                this.setBaby(true);
+            }
+
+        }
+
+
+        return entityData;
+    }
+
+    public static class LarryData implements EntityData {
+        public final boolean baby;
+
+        public LarryData(boolean baby) {
+            this.baby = baby;
+        }
     }
 }
